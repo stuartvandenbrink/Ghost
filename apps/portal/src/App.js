f689cbef59ebec1fe631e19107a1ab79cb3f14aa
Stuart VandenBrink <stuart.vandenbrink@sonarsource.com>
2025-09-08 14:27:14 -0500

    come on

--- apps/portal/src/App.js
+++ apps/portal/src/App.js
@@ -28,6 +28,7 @@ const DEV_MODE_DATA = {
 
 function SentryErrorBoundary({site, children}) {
     console.log("hello from the inside");
+    console.log("PLEASE");
     console.log("goodbye, yall");
     const {portal_sentry: portalSentry} = site || {};
     if (portalSentry && portalSentry.dsn) {
